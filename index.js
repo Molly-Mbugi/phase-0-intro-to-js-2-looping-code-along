9bae0abd9a11d881f53cc7775ef1f70f8c7ef6a9
molly <mollynzembi@gmail.com>
2024-03-25 23:06:45 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+}
+
+// Function to count down from a provided number to zero
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
